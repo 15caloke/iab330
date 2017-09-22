5906fb116430eea78f19e3aedb2e017b2e1c94c1
JedRiggs <timothyjedriggs@gmail.com>
2017-09-22 14:47:33 +1000

    Version 1.1

---
+++ StoragePal1.1/StoragePal1.1/StoragePal1.1Page.xaml.cs
@@ -0,0 +1,12 @@
+﻿using Xamarin.Forms;
+
+namespace StoragePal1
+{
+    public partial class StoragePal1_1Page : TabbedPage
+    {
+        public StoragePal1_1Page()
+        {
+            InitializeComponent();
+        }
+    }
+}
