248875e044a614cfc33c7ff38f8b3fddf4ce32b9
15caloke <calum.oke@connect.qut.edu.au>
2017-10-28 15:15:05 +1000

    Add New Room Page Added

---
+++ StoragePal1.1/StoragePal1.1/SubPages/AddRoomPage.xaml.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace StoragePal1.SubPages {
+    public partial class AddRoomPage : ContentPage {
+        public AddRoomPage() {
+            InitializeComponent();
+            BindingContext = new MainViewModel();
+        }
+
+        private void SubmitRoom_Clicked(object sender, EventArgs e) {
+            Navigation.PopAsync(true);
+        }
+    }
+}
